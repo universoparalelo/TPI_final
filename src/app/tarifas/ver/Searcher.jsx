5b584060815711fdb0bfe6dad503be9512836d94
yoelmarain <jmarain62@gmail.com>
2024-11-24 00:11:02 -0300

    fix: ajustado tabla padding para de tarifas para ver todos los datos de una

--- src/app/tarifas/ver/Searcher.jsx
+++ src/app/tarifas/ver/Searcher.jsx
@@ -5,7 +5,7 @@ export default function Searcher({ query, setQuery }) {
 
   return (
     <div className="p-4">
-        <input type="text" value={query} onChange={handleSearch} placeholder="Buscar por nombre" className="w-full p-2 border border-gray-300 rounded"/>
+        <input type="text" value={query} onChange={handleSearch} placeholder="Buscar por vehiculo" className="w-full p-2 border border-gray-300 rounded"/>
     </div>
   );
 }
--- src/app/tarifas/ver/Table.jsx
+++ src/app/tarifas/ver/Table.jsx
@@ -4,23 +4,23 @@ export default function Table({ tarifasMostradas }) {
             <table className="min-w-full bg-white border border-gray-200 rounded-lg">
                 <thead className="bg-gray-100 text-gray-600 text-sm">
                     <tr>
-                        <th className="text-left py-3 px-20 font-medium w-1/5">Vehículo</th>
-                        <th className="text-left py-3 px-20 font-medium w-1/5">Marca</th>
-                        <th className="text-left py-3 px-20 font-medium w-1/5">Modelo</th>
-                        <th className="text-left py-3 px-20 font-medium w-1/5">Categoría</th>
-                        <th className="text-left py-3 px-20 font-medium w-1/5">Frecuencia</th>
-                        <th className="text-left py-3 px-20 font-medium w-1/5">Precio</th>
+                        <th className="text-left py-3 px-5 font-medium w-1/5">Vehículo</th>
+                        <th className="text-left py-3 px-5 font-medium w-1/5">Marca</th>
+                        <th className="text-left py-3 px-5 font-medium w-1/5">Modelo</th>
+                        <th className="text-left py-3 px-5 font-medium w-1/5">Categoría</th>
+                        <th className="text-left py-3 px-5 font-medium w-1/5">Frecuencia</th>
+                        <th className="text-left py-3 px-5 font-medium w-1/5">Precio</th>
                     </tr>
                 </thead>
                 <tbody className="text-gray-700 text-sm divide-y divide-gray-200">
                     {tarifasMostradas.map((tarifa, index) => (
                     <tr key={index}>
-                        <td className="text-left py-3 px-20">{tarifa.vehiculo}</td>
-                        <td className="text-left py-3 px-20">{tarifa.marca}</td>
-                        <td className="text-left py-3 px-20">{tarifa.modelo}</td>
-                        <td className="text-left py-3 px-20">{tarifa.categoria}</td>
-                        <td className="text-left py-3 px-20">{tarifa.frecuencia}</td>
-                        <td className="text-left py-3 px-20">{tarifa.precio}</td>
+                        <td className="text-left py-3 px-5">{tarifa.vehiculo}</td>
+                        <td className="text-left py-3 px-5">{tarifa.marca}</td>
+                        <td className="text-left py-3 px-5">{tarifa.modelo}</td>
+                        <td className="text-left py-3 px-5">{tarifa.categoria}</td>
+                        <td className="text-left py-3 px-5">{tarifa.frecuencia}</td>
+                        <td className="text-left py-3 px-5">{tarifa.precio}</td>
                         {/* <td className="py-3 px-4 text-blue-600 hover:underline cursor-pointer">Edit</td> */}
                     </tr>
                     ))}
