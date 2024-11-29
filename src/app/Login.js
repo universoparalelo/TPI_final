84bd8ae74fe1017b83230198354fba05d70c64d0
yoelmarain <jmarain62@gmail.com>
2024-11-29 20:11:23 -0300

    Refactor Login component layout and enhance button styles

--- src/app/Login.js
+++ src/app/Login.js
@@ -64,18 +64,23 @@ export default function Login({ onClose, onLoginSuccess }) {
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
-          <div className="flex justify-between mt-10 items-center ">
+          <div className="mb-4 flex justify-end">
+            <Link href="/forgot-password" className="text-sm text-indigo-600 hover:underline">
+              ¿Olvidó su contraseña?
+            </Link>
+          </div>
+          <div className="flex justify-end mt-10 items-center gap-4">
             <button
               type="button"
               onClick={onClose}
-              className="text-sm font-medium text-gray-700 hover:text-gray-900"
+              className=" items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md shadow-sm text-black hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
             >
               Cancelar
             </button>
             <button
               type="submit"
               onClick={checkLogin}
-              className="ml-4 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className=" items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Acceder
             </button>
