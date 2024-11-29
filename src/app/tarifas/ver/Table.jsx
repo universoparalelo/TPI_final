7b7a0d6f11ae223771dad1c0722eefa6f65bd96c
yoelmarain <jmarain62@gmail.com>
2024-11-29 20:02:17 -0300

    Refactor Table component to remove 'Marca' column and adjust column widths

--- src/app/tarifas/ver/Table.jsx
+++ src/app/tarifas/ver/Table.jsx
@@ -5,7 +5,7 @@ export default function Table({ tarifasMostradas }) {
                 <thead className="bg-gray-100 text-gray-600 text-sm">
                     <tr>
                         <th className="text-left py-3 px-5 font-medium w-1/5">Vehículo</th>
-                        <th className="text-left py-3 px-5 font-medium w-1/5">Marca</th>
+                        {/* <th className="text-left py-3 px-5 font-medium w-1/5">Marca</th> */}
                         <th className="text-left py-3 px-5 font-medium w-1/5">Modelo</th>
                         <th className="text-left py-3 px-5 font-medium w-1/5">Categoría</th>
                         <th className="text-left py-3 px-5 font-medium w-1/5">Frecuencia</th>
@@ -15,12 +15,12 @@ export default function Table({ tarifasMostradas }) {
                 <tbody className="text-gray-700 text-sm divide-y divide-gray-200">
                     {tarifasMostradas.map((tarifa, index) => (
                     <tr key={index}>
-                        <td className="text-left py-3 px-5">{tarifa.vehiculo}</td>
-                        <td className="text-left py-3 px-5">{tarifa.marca}</td>
-                        <td className="text-left py-3 px-5">{tarifa.modelo}</td>
-                        <td className="text-left py-3 px-5">{tarifa.categoria}</td>
-                        <td className="text-left py-3 px-5">{tarifa.frecuencia}</td>
-                        <td className="text-left py-3 px-5">{tarifa.precio}</td>
+                        <td className="text-left py-3 px-5  w-1/5">{tarifa.vehiculo}</td>
+                        {/* <td className="text-left py-3 px-5">{tarifa.marca}</td> */}
+                        <td className="text-left py-3 px-5  w-1/5">{tarifa.modelo}</td>
+                        <td className="text-left py-3 px-5  w-1/5">{tarifa.categoria}</td>
+                        <td className="text-left py-3 px-5  w-1/5">{tarifa.frecuencia}</td>
+                        <td className="text-left py-3 px-5 w-1/5">{tarifa.precio},00</td>
                         {/* <td className="py-3 px-4 text-blue-600 hover:underline cursor-pointer">Edit</td> */}
                     </tr>
                     ))}
